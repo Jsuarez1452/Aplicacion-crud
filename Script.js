bbcf4d700374949cc6a8b4cbefe25f7d090988bf
Jsuarez1452 <jsuarez1452@outlook.com>
2025-08-27 19:59:59 -0600

    Hora de la cena :D

--- Script.js
+++ Script.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const activityInput = document.getElementById('activityInput');
     const addActivityButton = document.getElementById('addActivityButton');
     const activityList = document.getElementById('activityList');
+    const emptyImage = document.querySelector('.emptyImage');
     
+    const toggleEmptyImage = () => {
+       emptyImage.style.display = activityList.children.length === 0 ? 'block' : 'none';
+    };
+
     const addActivity = (event) => {
         event.preventDefault();
         const activityText = activityInput.value.trim();
@@ -11,9 +16,18 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
 
         const listItem = document.createElement('li');
-        listItem.textContent = activityText;
+        listItem.innerHTML = `
+            <input type="checkbox" class="checkBox">
+            <span>${activityText}</span>
+            <div class="editDeleteButtons">
+                <button class="editButton"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button class="deleteButton"><i class="fa-solid fa-trash"></i></button>
+            </div>
+        `;
+
         activityList.appendChild(listItem);
         activityInput.value = '';
+        toggleEmptyImage();
     };
 
     addActivityButton.addEventListener('click', addActivity);
