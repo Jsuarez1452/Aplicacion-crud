ffeeee509e2d8bfc2b04ed2480e19ff2bd7e26bb
Jsuarez1452 <jsuarez1452@outlook.com>
2025-08-27 18:24:27 -0600

    Primer javascript

--- Script.js
+++ Script.js
@@ -1,2 +1,24 @@
-//<i class="fa-solid fa-pencil"></i>
-//<i class="fa-solid fa-trash"></i>
+
+document.addEventListener('DOMContentLoaded', () => {
+    const activityInput = document.getElementById('activityInput');
+    const addActivityButton = document.getElementById('addActivityButton');
+    const activityList = document.getElementById('activityList');
+    
+    const addActivity = (event) => {
+        event.preventDefault();
+        const activityText = activityInput.value.trim();
+        if (!activityText) 
+            return;
+
+        const listItem = document.createElement('li');
+        listItem.textContent = activityText;
+        activityList.appendChild(listItem);
+        activityInput.value = '';
+    };
+
+    addActivityButton.addEventListener('click', addActivity);
+    activityInput.addEventListener ('keypress', (e) => {
+        if (e.key === 'Enter') 
+            addActivity(e);
+    });
+});
